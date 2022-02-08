6e6471167055833a9b1ba01e01774218e56d6038
Tito-74 <kipkirui133@gmail.com>
2022-02-08 22:13:47 +0300

    creating models and serializers

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Store, Book, Author
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(Book)
+admin.site.register(Author)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .serializers import StoreSerializer
+from .models import Store
+from rest_framework import viewsets
 # Create your views here.
+
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
